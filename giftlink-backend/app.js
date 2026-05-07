4b5300d6563167fd7af6c2a05fd026d9e6b80d76
Julia Zdzitowiecka <msj.desart@gmail.com>
2026-05-07 15:32:29 +0200

    Create API for Search Service

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -27,8 +27,8 @@ app.use(express.json());
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
-
+app.use('/api/gifts', giftRoutes);
+app.use('/api/search', searchRoutes);
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -1,38 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
+const logger = require('../logger');
 
 // Search for gifts
 router.get('/', async (req, res, next) => {
     try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
-        // {{insert code here}}
-
+        // Connect to MongoDB using connectToDatabase database
+        const db = await connectToDatabase();
         const collection = db.collection("gifts");
 
         // Initialize the query object
         let query = {};
 
-        // Add the name filter to the query if the name parameter is not empty
-        // if (/* {{insert code here}} */) {
+        if (req.query.name && req.query.name.trim() !== "") {
             query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
-        // }
-
-        // Task 3: Add other filters to the query
-        if (req.query.category) {
-            // {{insert code here}}
         }
-        if (req.query.condition) {
-            // {{insert code here}} 
+        if (req.query.category && req.query.category.trim() !== "") {
+            query.category = { $regex: req.query.category, $options: "i" };
         }
-        if (req.query.age_years) {
-            // {{insert code here}}
-            query.age_years = { $lte: parseInt(req.query.age_years) };
+        if (req.query.condition && req.query.condition.trim() !== "") {
+            query.condition = { $regex: req.query.condition, $options: "i" };
         }
-
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        // {{insert code here here}}
-
+        if (req.query.age_years && !isNaN(req.query.age_years)) {
+            query.age_years = { $lte: Number(req.query.age_years) };
+        }
+        logger.info(`Search query: ${JSON.stringify(query)}`);
+        // Fetch filtered gifts
+        const gifts = await collection.find(query).toArray();
+        
         res.json(gifts);
     } catch (e) {
         next(e);
