0d65be590f1a6176b259ea99853103d26aa6c93f
Julia Zdzitowiecka <msj.desart@gmail.com>
2026-05-07 15:44:05 +0200

    Create API for Search Service and upgrade app

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -2,38 +2,30 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
+const pinoHttp = require('pino-http');
+const logger = require('./logger');
 const giftRoutes = require('./routes/giftRoutes');
 const searchRoutes = require('./routes/searchRoutes');
 
 const app = express();
-app.use("*",cors());
 const port = 3060;
 
-// Connect to MongoDB; we just do this one time
-connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
-})
-    .catch((e) => console.error('Failed to connect to DB', e));
+// Connect to DB once
+connectToDatabase()
+    .then(() => logger.info('Connected to DB'))
+    .catch((e) => logger.error('Failed to connect to DB', e));
 
-
+app.use(pinoHttp({ logger }));
+app.use(cors());
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-app.use('/api/gifts', giftRoutes);
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 app.use('/api/gifts', giftRoutes);
 app.use('/api/search', searchRoutes);
 
-const pinoHttp = require('pino-http');
-const logger = require('./logger');
-
-app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
@@ -42,16 +34,16 @@ app.use(pinoHttp({ logger }));
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
 
+// Root
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
 
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
-
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
