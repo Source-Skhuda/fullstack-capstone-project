e3c537a4c37abe5ee31802c3f6a504bb91e7d997
Julia Zdzitowiecka <msj.desart@gmail.com>
2026-05-07 14:26:11 +0200

    Implement the /api/gifts Endpoint

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -6,7 +6,8 @@ const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
-const Routes = require('./routes/giftRoutes');
+const giftRoutes = require('./routes/giftRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 
 const app = express();
 app.use("*",cors());
@@ -23,7 +24,7 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
